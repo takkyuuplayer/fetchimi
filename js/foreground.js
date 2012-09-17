0a86dab6152c7553d7246af043416b5d3c8ee86b
takkyuuplayer <takkyuuplayer@gmail.com>
2012-09-18 00:42:21 +0900

    単語探索まで

--- js/foreground.js
+++ js/foreground.js
@@ -1,3 +1,4 @@
+var before = '';
 $(function() {
 	$('body').append('<div id="fetchimi-window">');
 	$('body').on('click', function() {
@@ -9,4 +10,47 @@ $(function() {
 			}
 		});
 	});
+	$('body').mousemove(function(e) {
+		var range = document.caretRangeFromPoint(e.clientX, e.clientY);
+		var node = range.startContainer;
+		var onmousetext = node.textContent;
+		var mouseoffset = range.startOffset;
+		lTime = Date.now();
+		setTimeout(function(t) {
+			if (t != lTime) {
+				return;
+			}
+			try {
+				if (node.nodeType != Node.TEXT_NODE)
+					return;
+			} catch (e) {
+				return;
+			}
+			var rng = getWordRange(node, mouseoffset);
+			if (rng == null || before == rng.toString())
+				return;
+			pageX = e.pageX;
+			pageY = e.pageY;
+			before = rng.toString();
+			alert(before);
+		}, 500, lTime);
+	});
 });
+function getWordRange(textNode, n) {
+	var words = textNode.wholeText.split(/(\b)/i);
+	var a = b = -1;
+	for ( var i = x = 0; i < words.length; i++)
+		if (n < (x += words[i].length)) {
+			if (/^\w+$/i.test(words[i])) {
+				a = x - words[i].length;
+				b = x;
+			}
+			break;
+		}
+	if (a == -1 || b == -1)
+		return null;
+	var rng = document.createRange();
+	rng.setStart(textNode, a);
+	rng.setEnd(textNode, b);
+	return rng;
+}
