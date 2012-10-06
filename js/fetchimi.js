bc7ab00196b46281116f895da8a2da2ce75d8d7b
takkyuuplayer <takkyuuplayer@gmail.com>
2012-10-06 21:55:47 +0900

    Refactoring

--- js/fetchimi.js
+++ js/fetchimi.js
@@ -1,59 +1,60 @@
+//<script type="test/javascript">
 var FetchImi = FetchImi || {};
 FetchImi.Alc = Backbone.Model.extend({
-	defaults: {
-		url: "http://eow.alc.co.jp/search",
-		word: "test",
-		$detail: null
-	},
-	getMidashi: function() {
-		if(!this.get("$detail")) {
-			return null;
-		}
-	  	return this.get("$detail").find("span.midashi").text();
-	},
-	isFind: function() {
-		if(!this.get("$detail")) {
-			return false;
-		}
-		var reg = new RegExp("^"+this.get("word")+"$", "i");
-		return reg.test(this.getMidashi());
-	},
-	getPron: function() {
-		if(!this.get("$detail")) {
-			return null;
-		}
-		return this.get("$detail").find("span.pron").text();
-	},
-	getDescriptions: function() {
-		if(!this.get("$detail")) {
-			return null;
-		}
-		var classes  = this.get("$detail").find("div span.wordclass");
-		var meanings = this.get("$detail").find("div ol");
-		var descs = [];
-		classes.each(function(i) {
-			descs.push({class:$(this).html(), meaning:$(meanings[i]).html()});
-		});
-		return descs;
-	},
-	fetch: function() {
-	  	return $.get(this.get("url"), {q: this.get("word")}).done($.proxy(function(html) {
-	  		this.set("$detail", $(html).find("#resultsList ul li:first-child"));
-	  	}, this));
-	}
+    defaults: {
+        url: "http://eow.alc.co.jp/search",
+        word: "test",
+        $detail: null
+    },
+    getMidashi: function() {
+        if(!this.get("$detail")) {
+            return null;
+        }
+          return this.get("$detail").find("span.midashi").text();
+    },
+    isFind: function() {
+        if(!this.get("$detail")) {
+            return false;
+        }
+        var reg = new RegExp("^"+this.get("word")+"$", "i");
+        return reg.test(this.getMidashi());
+    },
+    getPron: function() {
+        if(!this.get("$detail")) {
+            return null;
+        }
+        return this.get("$detail").find("span.pron").text();
+    },
+    getDescriptions: function() {
+        if(!this.get("$detail")) {
+            return null;
+        }
+        var classes  = this.get("$detail").find("div span.wordclass");
+        var meanings = this.get("$detail").find("div ol");
+        var descs = [];
+        classes.each(function(i) {
+            descs.push({class:$(this).html(), meaning:$(meanings[i]).html()});
+        });
+        return descs;
+    },
+    fetch: function() {
+          return $.get(this.get("url"), {q: this.get("word")}).done($.proxy(function(html) {
+              this.set("$detail", $(html).find("#resultsList ul li:first-child"));
+          }, this));
+    }
 });
 var alc = new FetchImi.Alc();
 chrome.extension.onConnect.addListener(function(port) {
   console.assert(port.name === "FetchImi.Alc");
   port.onMessage.addListener(function(msg) {
-  	if(! msg.word) {
-  		return;
-  	}
-  	alc.set("word", msg.word);
-  	alc.fetch().done(function() {
-  		if( alc.isFind() ) {
-	  		port.postMessage({status: "find", detail:alc.get("$detail").html()});
-	  	}
-  	});
+      if(! msg.word) {
+          return;
+      }
+      alc.set("word", msg.word);
+      alc.fetch().done(function() {
+          if( alc.isFind() ) {
+              port.postMessage({status: "find", detail:alc.get("$detail").html()});
+          }
+      });
   });
 });
--- js/foreground.js
+++ js/foreground.js
@@ -1,68 +1,69 @@
+//<script type="test/javascript">
 (function($) {
 var before = '';
 var ITime = null;
 var pageX = 0, pageY = 0;
 $(function() {
-	$('body').append('<div id="fetchimi-window" style="display:none;">');
-	var port = chrome.extension.connect({name: "FetchImi.Alc"});
-		port.onMessage.addListener(function(msg) {
-			if(msg.status === "find") {
-				onText(msg.detail);
-			}
-		});
-	$('body').mousemove(function(e) {
-		$('#fetchimi-window').css('display', 'none');
-		var range = document.caretRangeFromPoint(e.clientX, e.clientY);
-		var node = range.startContainer;
-		var onmousetext = node.textContent;
-		var mouseoffset = range.startOffset;
-		// グローバル変数なので、マウスを動かすたびに変更される
-		lTime = Date.now();
-		setTimeout(function(t) {
-			if (t != lTime) {
-				return;
-			}
-			try {
-				if (node.nodeType != Node.TEXT_NODE)
-					return;
-			} catch (e) {
-				return;
-			}
-			var rng = getWordRange(node, mouseoffset);
-			if (rng == null || before == rng.toString()) {
-				return;
-			}
-			pageX = e.pageX;
-			pageY = e.pageY;
-			before = rng.toString();
-			port.postMessage({word: before});
-		}, 1000, lTime);
-	});
+    $('body').append('<div id="fetchimi-window" style="display:none;">');
+    var port = chrome.extension.connect({name: "FetchImi.Alc"});
+        port.onMessage.addListener(function(msg) {
+            if(msg.status === "find") {
+                onText(msg.detail);
+            }
+        });
+    $('body').mousemove(function(e) {
+        $('#fetchimi-window').css('display', 'none');
+        var range = document.caretRangeFromPoint(e.clientX, e.clientY);
+        var node = range.startContainer;
+        var onmousetext = node.textContent;
+        var mouseoffset = range.startOffset;
+        // グローバル変数なので、マウスを動かすたびに変更される
+        lTime = Date.now();
+        setTimeout(function(t) {
+            if (t != lTime) {
+                return;
+            }
+            try {
+                if (node.nodeType != Node.TEXT_NODE)
+                    return;
+            } catch (e) {
+                return;
+            }
+            var rng = getWordRange(node, mouseoffset);
+            if (rng == null || before == rng.toString()) {
+                return;
+            }
+            pageX = e.pageX;
+            pageY = e.pageY;
+            before = rng.toString();
+            port.postMessage({word: before});
+        }, 1000, lTime);
+    });
 });
 function getWordRange(textNode, n) {
-	var words = textNode.wholeText.split(/(\b)/i);
-	var a = b = -1;
-	for ( var i = x = 0; i < words.length; i++)
-		if (n < (x += words[i].length)) {
-			if (/^\w+$/i.test(words[i])) {
-				a = x - words[i].length;
-				b = x;
-			}
-			break;
-		}
-	if (a == -1 || b == -1)
-		return null;
-	var rng = document.createRange();
-	rng.setStart(textNode, a);
-	rng.setEnd(textNode, b);
-	return rng;
+    var words = textNode.wholeText.split(/(\b)/i);
+    var a = b = -1;
+    for ( var i = x = 0; i < words.length; i++)
+        if (n < (x += words[i].length)) {
+            if (/^\w+$/i.test(words[i])) {
+                a = x - words[i].length;
+                b = x;
+            }
+            break;
+        }
+    if (a == -1 || b == -1)
+        return null;
+    var rng = document.createRange();
+    rng.setStart(textNode, a);
+    rng.setEnd(textNode, b);
+    return rng;
 }
 function onText( detail ) {
     $("#fetchimi-window").ready(function() {
-    	$("#fetchimi-window").html(detail);
-    	$("#fetchimi-window").css('top', (pageY))
-    		.css('left', (pageX))
-    		.css('display', 'block');
+        $("#fetchimi-window").html(detail);
+        $("#fetchimi-window").css('top', (pageY))
+            .css('left', (pageX))
+            .css('display', 'block');
     });
 }
 })(jQuery);
