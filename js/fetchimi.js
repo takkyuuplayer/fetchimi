0f40fc444015335d0b77e35c3228883965ac7553
takkyuuplayer <takkyuuplayer@gmail.com>
2012-10-08 14:49:55 +0900

    Refactoring

--- js/fetchimi.js
+++ js/fetchimi.js
@@ -1,5 +1,5 @@
 //<script type="test/javascript">
-var FetchImi = FetchImi || {};
+var FetchImi = FetchImi || {}
 FetchImi.Alc = Backbone.Model.extend({
     defaults: {
         url: "http://eow.alc.co.jp/search",
@@ -9,63 +9,62 @@ FetchImi.Alc = Backbone.Model.extend({
     },
     getMidashi: function() {
         if(!this.get("$detail")) {
-            return null;
+            return null
         }
-          return this.get("$detail").find("span.midashi").text();
+          return this.get("$detail").find("span.midashi").text()
     },
     isFind: function() {
         if(!this.get("$detail")) {
-            return false;
+            return false
         }
-        var reg = new RegExp("^"+this.get("word")+"$", "i");
-        return reg.test(this.getMidashi());
+        var reg = new RegExp("^"+this.get("word")+"$", "i")
+        return reg.test(this.getMidashi())
     },
     hasVariation: function() {
         if(!this.get("$detail")) {
-            return false;
+            return false
         }
-        var word = this.get("$detail").find("div.sas a:first-child");
+        var word = this.get("$detail").find("div.sas a:first-child")
     },
     getPron: function() {
         if(!this.get("$detail")) {
-            return null;
+            return null
         }
-        return this.get("$detail").find("span.pron").text();
+        return this.get("$detail").find("span.pron").text()
     },
     getDescriptions: function() {
         if(!this.get("$detail")) {
-            return null;
+            return null
         }
-        var classes  = this.get("$detail").find("div span.wordclass");
-        var meanings = this.get("$detail").find("div ol");
-        var descs = [];
+        var classes  = this.get("$detail").find("div span.wordclass")
+        var meanings = this.get("$detail").find("div ol")
+        var descs = []
         classes.each(function(i) {
-            descs.push({class:$(this).html(), meaning:$(meanings[i]).html()});
-        });
-        return descs;
+            descs.push({class:$(this).html(), meaning:$(meanings[i]).html()})
+        })
+        return descs
     },
     fetch: function() {
         return $.get(this.get("url"), {q: this.get("word")})
             .done($.proxy(function(html) {
-                        this.set("$detail", $(html).find("#resultsList ul li:first-child"));
+                        this.set("$detail", $(html).find("#resultsList ul li:first-child"))
                         }, this))
-        ;
     }
-});
-var alc = new FetchImi.Alc();
+})
+var alc = new FetchImi.Alc()
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
       if(! msg.word) {
-          port.postMessage({status: "not found"});
-          return;
+          port.postMessage({status: "not found"})
+          return
       }
-      alc.set("word", msg.word);
+      alc.set("word", msg.word)
       alc.fetch().done(function() {
           if( alc.isFind() ) {
-              port.postMessage({status: "find", detail:alc.get("$detail").html()});
-              return;
+              port.postMessage({status: "find", detail:alc.get("$detail").html()})
+              return
           }
-          port.postMessage({status: "not found"});
-      });
-  });
-});
+          port.postMessage({status: "not found"})
+      })
+  })
+})
--- js/foreground.js
+++ js/foreground.js
@@ -1,13 +1,13 @@
 //<script type="test/javascript">
 (function($) {
 var clientX = clientY = 0
-var FetchImi = FetchImi || {};
+var FetchImi = FetchImi || {}
 FetchImi.Foreground = function() {
-    this.port = chrome.extension.connect({name: "FetchImi.Alc"});
-    this.bindEvents();
-    $('body').append('<div id="fetchimi-window" style="display:none;">');
-    this.lTime = 0;
-};
+    this.port = chrome.extension.connect({name: "FetchImi.Alc"})
+    this.bindEvents()
+    $('body').append('<div id="fetchimi-window" style="display:none;">')
+    this.lTime = 0
+}
 
 FetchImi.Foreground.prototype = {
     before: null,
@@ -18,7 +18,7 @@ FetchImi.Foreground.prototype = {
             .css('display', 'none')
     },
     showProgress: function(message) {
-        this.resetView();
+        this.resetView()
         $("#fetchimi-window").text(message)
             .addClass('progress')
             .css('top', (this.calcTop()))
@@ -26,7 +26,7 @@ FetchImi.Foreground.prototype = {
             .css('display', 'block')
     },
     showDetail: function(detail) {
-        this.resetView();
+        this.resetView()
         $("#fetchimi-window").html(detail)
             .css('top', this.calcTop())
             .css('left', this.calcLeft())
@@ -36,9 +36,9 @@ FetchImi.Foreground.prototype = {
         var height = $("#fetchimi-window").outerHeight()
         var top = clientY + height > $(window).height()
                 ? Math.max(0, $(window).height() - height - this.relative)
-                : clientY - this.relative;
+                : clientY - this.relative
 
-        return top;
+        return top
     },
     calcLeft: function() {
         var width = $("#fetchimi-window").outerWidth()
@@ -47,82 +47,82 @@ FetchImi.Foreground.prototype = {
                 ? Math.max(0, clientX - width - this.relative)
                 : clientX + this.relative
 
-        return left;
+        return left
     },
     bindEvents: function() {
-        var self = this;
+        var self = this
         $('body').on('mousemove', function(e) {
-            $('#fetchimi-window').css('display', 'none');
-        });
+            $('#fetchimi-window').css('display', 'none')
+        })
         $('body').on('mousemove', function(e) {
-            clientX = e.clientX;
-            clientY = e.clientY;
-        });
+            clientX = e.clientX
+            clientY = e.clientY
+        })
         $('body').on('mousemove', function(e) {
             self.resetView()
-            lTime = Date.now();
+            lTime = Date.now()
             setTimeout(function(t) {
                 if (t !== lTime) {
-                    return;
+                    return
                 }
-                var word = self.findWord();
+                var word = self.findWord()
                 if(self.before === word) {
-                    return;
+                    return
                 }
-                self.before = word;
-                self.showProgress('Searching...');
-                self.port.postMessage({word: word});
-            }, 1000, lTime);
-        });
+                self.before = word
+                self.showProgress('Searching...')
+                self.port.postMessage({word: word})
+            }, 1000, lTime)
+        })
         this.port.onMessage.addListener(function(msg) {
             self.resetView()
             if(msg.status === "find") {
-                self.showDetail(msg.detail);
+                self.showDetail(msg.detail)
             } else if(msg.status === 'not found') {
-                self.showProgress('Not Found');
+                self.showProgress('Not Found')
             }
 
-        });
+        })
     },
     findWord: function() {
-        var range = document.caretRangeFromPoint(clientX, clientY);
-        var node = range.startContainer;
-        var onmousetext = node.textContent;
-        var mouseoffset = range.startOffset;
+        var range = document.caretRangeFromPoint(clientX, clientY)
+        var node = range.startContainer
+        var onmousetext = node.textContent
+        var mouseoffset = range.startOffset
 
         try {
             if (node.nodeType != Node.TEXT_NODE)
-            return;
+            return
         } catch (e) {
-            return;
+            return
         }
-        var rng = this.getWordRange(node, mouseoffset);
+        var rng = this.getWordRange(node, mouseoffset)
         if (rng === null) {
-            return;
+            return
         }
 
-        return rng.toString();
+        return rng.toString()
     },
     getWordRange: function(textNode, n) {
-        var words = textNode.wholeText.split(/(\b)/i);
-        var a = b = -1;
+        var words = textNode.wholeText.split(/(\b)/i)
+        var a = b = -1
         for ( var i = x = 0; i < words.length; i++)
         if (n < (x += words[i].length)) {
             if (/^\w+$/i.test(words[i])) {
-                a = x - words[i].length;
-                b = x;
+                a = x - words[i].length
+                b = x
             }
-            break;
+            break
         }
         if (a == -1 || b == -1)
-        return;
-        var rng = document.createRange();
-        rng.setStart(textNode, a);
-        rng.setEnd(textNode, b);
-        return rng;
+        return
+        var rng = document.createRange()
+        rng.setStart(textNode, a)
+        rng.setEnd(textNode, b)
+        return rng
     }
 }
 $(function() {
-    var foreground = new FetchImi.Foreground();
-});
-})(jQuery);
+    var foreground = new FetchImi.Foreground()
+})
+})(jQuery)
